9877304f0c6415ae8979e5cc13a49559155fdd9d
sanket <sankethalake@gmail.com>
2021-08-14 11:56:23 +0530

    Read only model Viewset

--- ReadOnlyModelViewset/ReadOnlyModelViewset/urls.py
+++ ReadOnlyModelViewset/ReadOnlyModelViewset/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from api import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+router.register('studentapi',views.StudentReadOnlyModelViewset, basename='Student')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include(router.urls)),
 ]
